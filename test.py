7b4fc6ace9669de4e25f8b1391b9ad6c754ad855
am230 <111672334+am230@users.noreply.github.com>
2023-06-27 19:43:51 +0900

    hm

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
+
 import strinpy
+
 condition = True
-output = strinpy.build(['a', condition and 'b', condition and ['text'], condition and (lambda: 'supplier')])
+output = strinpy.build([
+    'text\n',
+    condition and 'condition text\n',
+    condition and ['condition text list\n'],
+    condition and (lambda: 'supplier text\n'),
+    [
+        'text list\n',
+        [
+            'in text list\n'
+        ]
+    ]
+])
 print(output)
